22dd252ae86a127dfcf23dcbd4c67163032b5317
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-11-04 13:27:06 +0100

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Raleway: ["Raleway", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        secondary: "#7C4EE4",
+        primary: "#666666",
+        primary1: "#999999",
+        text: "#333333",
+        bg: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
